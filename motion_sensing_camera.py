80f8426cfb936e70c434818977217f20901813a6
James Neisen <122641269+james-orion@users.noreply.github.com>
2023-11-26 13:24:22 -0500

    Got the camera working

--- motion_sensing_camera.py
+++ motion_sensing_camera.py
@@ -11,22 +11,22 @@ pir = MotionSensor(4) #the PIR Sensor is connected to GPIO 4
 # user information about when the image was taken
 pir.wait_for_motion()
 camera.start_preview()
-sleep(5)
+sleep(0.5)
 
 dt_string = datetime.now().strftime('%d_%m_%Y_%H.%M.%S')
-filename1 = ('~/Desktop/CS2210/FINAL_PROJECT/images/' + dt_string + '.jpeg')
+filename1 = ('/home/james/Desktop/CS2210/FINAL_PROJECT/images/' + dt_string + '1.jpeg')
 camera.capture(filename1)
 print("Image Captured") #line to make sure the motion sensing is working
 
-sleep(1)
+sleep(0.5)
 dt_string = datetime.now().strftime('%d_%m_%Y_%H.%M.%S')
-filename2 = ('home/james/Desktop/CS2210/FINAL_PROJECT/images/' + dt_string + '.jpeg')
+filename2 = ('/home/james/Desktop/CS2210/FINAL_PROJECT/images/' + dt_string + '2.jpeg')
 camera.capture(filename2)
 print("Image Captured")
 
-sleep(1)
+sleep(0.5)
 dt_string_3 = datetime.now().strftime('%d_%m_%Y_%H.%M.%S')
-filename3 = ('home/james/Desktop/CS2210/FINAL_PROJECT/images/' + dt_string + '.jpeg')
+filename3 = ('/home/james/Desktop/CS2210/FINAL_PROJECT/images/' + dt_string + '3.jpeg')
 camera.capture(filename3)
 print("Image Captured")
 camera.stop_preview()
